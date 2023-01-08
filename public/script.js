69cb79e8ff8825887916517045cc2944e6ad4743
kramrekwob <m.bowker422@gmail.com>
2023-01-08 11:50:22 -0500

    Still Trying to get POST to work
    Not sure why a basic fetch request is not being received by the server

---
+++ public/script.js
@@ -0,0 +1,61 @@
+// import { autocomplete } from '@algolia/autocomplete-js';
+
+// const { request } = require("express");
+
+// import '@algolia/autocomplete-theme-classic';
+const requestform =  document.querySelector('form');
+const results = document.querySelector('#results')
+const formData = new FormData(requestform);
+const seeds = document.querySelector('.seeds');
+requestform.addEventListener('submit', formSubmitted);
+
+//on submit gather all final slider paramaters, the seeds, and GET from api
+function formSubmitted(event) {
+  event.preventDefault();
+  let params = [];
+  for ( var i = 0; i < requestform.elements.length; i++ ) {
+    console.log(requestform.elements[i])
+    params.push(requestform.elements[i].name + '=' +  requestform.elements[i].value);
+ }
+  getResults();
+}
+
+let sliders = document.querySelectorAll('input[type=range]');
+
+for (let i=0; i<sliders.length; i++){
+  sliders[i].addEventListener('change', function() {
+    sliders[i].nextElementSibling.innerHTML = sliders[i].value
+    // console.log(sliders[i].value);
+  })
+}
+
+// const popularitySlider = document.getElementById("popularity")
+// popularitySlider.oninput = function() {
+//   newValue = popularitySlider.ariaValueMax;
+//   document.getElementById("popdisplay").innerHTML = this.value;
+// };
+
+let query = "https://api.spotify.com/v1/recommendations/?limit=10&market=ES&";
+
+function getResults() {
+  for (let i=0; i<requestform.elements.length; i++){
+    query += '&' + requestform.elements[i].name + '=' +  requestform.elements[i].value;
+    // console.log(query)
+  }
+  fetch('/recommend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: JSON.stringify(query)
+    });
+}
+
+// }
+// autocomplete({
+//   container: '#autocomplete',
+//   placeholder: 'Search for products',
+//   getSources() {
+//     return [];
+//   },
+// });
--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const app = express();
 var http = require('http');
 var request = require('request'); // "Request" librarynpm
 require("dotenv").config({ path: "./config/.env" })
+
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+
 var client_id = '80af750695d24965b96b84c1b2b0665b'; // Your client id
 var client_secret = '6415a1e2f2c946fb85cd6d236d83d059'; // Your secret
 
-// your application requests authorization
+//let it use the public folder for scripts
+app.use(express.static("public"));
+
+// application requests authorization
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
@@ -19,33 +27,51 @@ var authOptions = {
   json: true
 };
 //receive access token as body.access_token
-
-// request.post(authOptions, function(error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       var token = body.access_token;
-//       tempToken=body.access_token;
-//       console.log(tempToken)
-//       i = tempToken;
-//     }
-//   });
+var tempToken;
 
 request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (error) console.log('initial authentication error')
+    if (!error && response.statusCode === 200) {
+      var token = body.access_token;
+      tempToken=body.access_token;
+      console.log(tempToken)
+      i = tempToken;
+    }
+  });
 
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: "https://api.spotify.com/v1/recommendations?limit=10&market=ES&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA",
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-        console.log(body)
-    });
-  }
-});
+app.post('/recommend', (req,res) => {
+  console.log(tempToken)
+  console.log(req)
+  // let recommend = {
+  //   url: req.body,
+  //   headers: {
+  //     'Authorization': 'Bearer ' + tempToken
+  //   },
+  //   json: true
+  // }
+  // request.get(recommend, function(error, response, body) {
+  //           console.log(body)
+  //       });
+})
+
+
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+
+//     // use the access token to access the Spotify Web API
+//     var token = body.access_token;
+//     var options = {
+//       url: "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+//       headers: {
+//         'Authorization': 'Bearer ' + token
+//       },
+//       json: true
+//     };
+//     request.get(options, function(error, response, body) {
+//         console.log(body)
+//     });
+//   }
+// });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -54,3 +80,5 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}, you better go catch it!`);
   });
+
+  
